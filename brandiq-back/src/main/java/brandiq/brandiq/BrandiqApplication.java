49af3d1257542909c9feb4fa735049e956bce6ac
Yxssssmin <yasmintate02@gmail.com>
2024-02-02 13:37:12 +0100

    Commit 20 de yasmin

--- brandiq-back/src/main/java/brandiq/brandiq/BrandiqApplication.java
+++ brandiq-back/src/main/java/brandiq/brandiq/BrandiqApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrandiqApplication {
 // Hola soy edu
+// Hola soy yasmin
 	public static void main(String[] args) {
 		SpringApplication.run(BrandiqApplication.class, args);
 	}
