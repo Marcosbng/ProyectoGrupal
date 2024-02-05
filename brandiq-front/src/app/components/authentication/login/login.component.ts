3e48c02b7b535bdfd929073fd71bcacb3d8dfa4c
HugoSanchis16 <hugolluissimarro03@gmail.com>
2024-02-05 17:48:58 +0100

    login Funcional

--- brandiq-front/src/app/components/authentication/login/login.component.ts
+++ brandiq-front/src/app/components/authentication/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+// login.component.ts
+import { Component, Injector } from '@angular/core';
+import { UsersService } from '../../../services/users.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  nickname: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private usersService: UsersService, private router: Router) {}
+
+  login() {
+    const user = { nickname: this.nickname, password: this.password };
+
+    // Utiliza el servicio para realizar la solicitud HTTP y manejar la lógica
+    this.usersService.login(user).subscribe({
+      next: (data) => {
+        console.log('Respuesta del servidor:', data);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Error en la solicitud:', error);
+        this.errorMessage =
+          'Error en el inicio de sesión. Verifica tus credenciales.';
+      },
+    });
+  }
 }
