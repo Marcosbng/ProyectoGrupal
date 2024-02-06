44721c69e3edf6de673d3fb1d456fe19b9c5617b
Yxssssmin <yasmintate02@gmail.com>
2024-02-06 09:40:21 +0100

    Registro

--- brandiq-front/src/app/services/users.service.ts
+++ brandiq-front/src/app/services/users.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, map } from 'rxjs';
+import { User } from '../interfaces/user';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  userSubject = new Subject<User>();
+
+  login(user: { 
+    nickname: string; 
+    password: string 
+  }): Observable<any> {
+    return this.http.post('http://localhost:8090/auth/login', user);
+  }
+
+  register(user: {
+    nickname: string,
+    nombre: string,
+    email: string,
+    password: string,
+    confirmPassword: string,
+  }): Observable<any> {
+     return this.http.post('http://localhost:8090/auth/nuevo', user);
+  }
 }
