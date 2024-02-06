e97d7c60dd85257ddc9a0b2a8bb4bc5a46fa2b69
unknown <HugoSanchis16>
2024-02-06 18:25:10 +0100

    Profile sin implementar

--- brandiq-front/src/app/services/users.service.ts
+++ brandiq-front/src/app/services/users.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,6 @@ export class UsersService {
   constructor(private http: HttpClient) {}
 
   login(user: { nickname: string; password: string }): Observable<any> {
-    return this.http.post('http://localhost:8090/auth/login', user);
+    return this.http.post(environment.URL_SPRING + 'auth/login', user);
   }
 }
