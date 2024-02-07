926f27887dcb7d4cc7009a841da70c20bc10a599
HugoSanchis16 <hugolluissimarro03@gmail.com>
2024-02-07 12:42:26 +0100

    token in localStorage

--- brandiq-front/src/app/components/header/header.component.ts
+++ brandiq-front/src/app/components/header/header.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UsersService } from '../../services/users.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, FormsModule],
+  imports: [RouterLink, RouterLinkActive, FormsModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(public userService: UsersService) {}
 
+  logout(): void {
+    this.userService.logout();
+  }
 }
